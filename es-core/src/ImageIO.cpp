51758cb5f7dac7cb9ecc9736ceb8df39aa2dd1c2
Bluestang <bluestang2006@gmail.com>
2021-01-17 17:48:55 -0700

    ImageIO Optimizations
    - Fixes

--- es-core/src/ImageIO.cpp
+++ es-core/src/ImageIO.cpp
@@ -10,19 +10,20 @@ std::vector<unsigned char> ImageIO::loadFromMemoryRGBA32(const unsigned char * d
 	width = 0;
 	height = 0;
 	FIMEMORY * fiMemory = FreeImage_OpenMemory((BYTE *)data, (DWORD)size);
-	if (fiMemory != nullptr) {
+	if (fiMemory != nullptr)
+	{
 		//detect the filetype from data
 		FREE_IMAGE_FORMAT format = FreeImage_GetFileTypeFromMemory(fiMemory);
-		if (format != FIF_UNKNOWN && FreeImage_FIFSupportsReading(format))
+		if (format != FIF_UNKNOWN && (FreeImage_FIFSupportsReading(format) != 0))
 		{
 			//file type is supported. load image
-			FIBITMAP * fiBitmap = FreeImage_LoadFromMemory(format, fiMemory);
+			FIBITMAP* fiBitmap = FreeImage_LoadFromMemory(format, fiMemory);
 			if (fiBitmap != nullptr)
 			{
 				//loaded. convert to 32bit if necessary
 				if (FreeImage_GetBPP(fiBitmap) != 32)
 				{
-					FIBITMAP * fiConverted = FreeImage_ConvertTo32Bits(fiBitmap);
+					FIBITMAP* fiConverted = FreeImage_ConvertTo32Bits(fiBitmap);
 					if (fiConverted != nullptr)
 					{
 						//free original bitmap data
@@ -30,34 +31,26 @@ std::vector<unsigned char> ImageIO::loadFromMemoryRGBA32(const unsigned char * d
 						fiBitmap = fiConverted;
 					}
 				}
-				if (fiBitmap != nullptr)
+				width = FreeImage_GetWidth(fiBitmap);
+				height = FreeImage_GetHeight(fiBitmap);
+				// loop through scanlines and add all pixel data to the return vector
+				// this is necessary, because width*height*bpp might not be == pitch
+				// do on-the-fly argb to abgr convertion
+				rawData.resize(width * height *4);
+				unsigned char* tempData = rawData.data();
+				int w = (int)width;
+				for (int y = (int)height; --y >= 0; )
 				{
-					width = FreeImage_GetWidth(fiBitmap);
-					height = FreeImage_GetHeight(fiBitmap);
-					//loop through scanlines and add all pixel data to the return vector
-					//this is necessary, because width*height*bpp might not be == pitch
-					unsigned char * tempData = new unsigned char[width * height * 4];
-					for (size_t i = 0; i < height; i++)
+					unsigned int* argb = (unsigned int*)FreeImage_GetScanLine(fiBitmap, y);
+					unsigned int* abgr = (unsigned int*)(tempData + (y * width * 4));
+					for (int x = w ; --x >= 0;)
 					{
-						const BYTE * scanLine = FreeImage_GetScanLine(fiBitmap, (int)i);
-						memcpy(tempData + (i * width * 4), scanLine, width * 4);
+						unsigned int c = argb[x];
+						abgr[x] = (c & 0xFF00FF00) | ((c & 0xFF) << 16) | ((c >> 16) & 0xFF);
 					}
-					//convert from BGRA to RGBA
-					for(size_t i = 0; i < width*height; i++)
-					{
-						RGBQUAD bgra = ((RGBQUAD *)tempData)[i];
-						RGBQUAD rgba;
-						rgba.rgbBlue = bgra.rgbRed;
-						rgba.rgbGreen = bgra.rgbGreen;
-						rgba.rgbRed = bgra.rgbBlue;
-						rgba.rgbReserved = bgra.rgbReserved;
-						((RGBQUAD *)tempData)[i] = rgba;
-					}
-					rawData = std::vector<unsigned char>(tempData, tempData + width * height * 4);
-					//free bitmap data
-					FreeImage_Unload(fiBitmap);
-					delete[] tempData;
 				}
+				//free bitmap data
+				FreeImage_Unload(fiBitmap);
 			}
 			else
 			{
@@ -76,15 +69,14 @@ std::vector<unsigned char> ImageIO::loadFromMemoryRGBA32(const unsigned char * d
 
 void ImageIO::flipPixelsVert(unsigned char* imagePx, const size_t& width, const size_t& height)
 {
-	unsigned int temp;
 	unsigned int* arr = (unsigned int*)imagePx;
-	for(size_t y = 0; y < height / 2; y++)
-	{
-		for(size_t x = 0; x < width; x++)
+	unsigned int* s1 = arr;
+	unsigned int* s2 = arr + (height - 1) * width;
+	for (int y = (int)height / 2; --y >= 0; s1 += width, s2 -= width)
+		for (int x = (int)width; --x >= 0; )
 		{
-			temp = arr[x + (y * width)];
-			arr[x + (y * width)] = arr[x + (height * width) - ((y + 1) * width)];
-			arr[x + (height * width) - ((y + 1) * width)] = temp;
+			unsigned int temp = s1[x];
+			s1[x] = s2[x];
+			s2[x] = temp;
 		}
-	}
 }
